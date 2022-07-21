78c45bd8b2d31355b86b28f22d074343f16ea9af
HarishSurya <38115933+HarishSurya@users.noreply.github.com>
2022-07-22 04:55:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,63 +9,86 @@ import Content from './components/main/content/content.component';
 
 import Loading from './components/loader/spinner.component';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      hasMore: true,
-      isLoading: true,
-      slicedItems: [],
-      data: [],
-    };
-  }
-
-  handleChange = (event) => {
-    this.setState({ searchField: event.target.value });
-  };
-
-  componentDidMount() {
-    axios('https://my-json-server.typicode.com/bidodev/api-insta/posts').then((res) => {
-      const slicedItems = res.data.slice(0, 9);
-      this.setState({ data: res.data, slicedItems });
-    });
-  }
-
-  fetchMoreData = () => {
-    const { data, slicedItems } = this.state;
-
-    if (slicedItems.length >= data.length) {
-      this.setState({ hasMore: false });
-      return;
-    }
-
-    setTimeout(() => {
-      const newItems = data.slice(slicedItems.length, slicedItems.length + 9);
-      this.setState({ slicedItems: slicedItems.concat(newItems) });
-    }, 1000);
-  };
-
-  render() {
-    const { isLoading, slicedItems } = this.state;
-
-    return (
-      <div className="App">
-        <Header handleChange={this.handleChange} />
-        <div className="main">
-          <Profile />
-          <InfiniteScroll
-            dataLength={this.state.slicedItems.length}
-            next={this.fetchMoreData}
-            hasMore={this.state.hasMore}
-            loader={<Loading/>}
-          >
-          <Content posts={slicedItems} videos={slicedItems} tags={slicedItems}/>
-          </InfiniteScroll>
+function App() {
+  return (
+    <div className="App">
+      <div id="wrapper">
+        <div className="container">
+          <div className="phone-app-demo" />
+          <div className="form-data">
+            <form action="">
+              <div className="logo">
+                <h1>Instagram</h1>
+              </div>
+              <input
+                type="text"
+                placeholder="Phone number, username, or email"
+              />
+              <input type="password" placeholder="Password" />
+              <button className="form-btn" type="submit">
+                Log in
+              </button>
+              <span className="has-separator">Or</span>
+              <a href="#" className="facebook-login">
+                <i className="fab fa-facebook" /> Log in with Facebook
+              </a>
+              <a className="password-reset" href="#">
+                Forgot password?
+              </a>
+            </form>
+            <div className="sign-up">
+              Don't an account? <a href="#">Sign up</a>
+            </div>
+            <div className="get-the-app">
+              <span>Get the app</span>
+              <div className="badge">
+                <img
+                  src="https://www.instagram.com/static/images/appstore-install-badges/badge_android_english-en.png/e9cd846dc748.png"
+                  alt="android App"
+                />
+                <img
+                  src="https://www.instagram.com/static/images/appstore-install-badges/badge_ios_english-en.png/180ae7a0bcf7.png"
+                  alt="ios app"
+                />
+              </div>
+            </div>
+          </div>
         </div>
+
+        <footer>
+          <div className="container">
+            <nav className="footer-nav">
+              <ul>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Support</a>
+                </li>
+                <li>
+                  <a href="#">Jobs</a>
+                </li>
+                <li>
+                  <a href="#">Privacy</a>
+                </li>
+                <li>
+                  <a href="#">Terms</a>
+                </li>
+                <li>
+                  <a href="#">Profiles</a>
+                </li>
+                <li>
+                  <a href="#">Languages</a>
+                </li>
+              </ul>
+            </nav>
+            <div className="copyright-notice">&copy; 2019 Complaints</div>
+          </div>
+        </footer>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
